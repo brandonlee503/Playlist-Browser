51e71f9f40005ea5ad1dc046446634cfad0bae28
Brandon <leebran@onid.oregonstate.edu>
2015-06-09 21:09:52 -0700

    Initial Commit

---
+++ Treehouse Playlist Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Treehouse Playlist Browser
+//
+//  Created by Brandon Lee on 6/9/15.
+//  Copyright (c) 2015 Brandon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
